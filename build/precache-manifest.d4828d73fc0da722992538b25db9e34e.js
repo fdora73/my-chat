8640e0f0c762b3c9b918548fd145da80259f8c40
Földi Dóra <dora.foldi73@gmail.com>
2020-05-22 17:17:55 +0200

    Kész megoldás build-del

---
+++ build/precache-manifest.d4828d73fc0da722992538b25db9e34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c7ef6d6ddc691b68cd92da039c7173",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caa92934aa29331576be",
+    "url": "./static/css/main.b9b8f7b4.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "./static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa92934aa29331576be",
+    "url": "./static/js/main.30e246de.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
